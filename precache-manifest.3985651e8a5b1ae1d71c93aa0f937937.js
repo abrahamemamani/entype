f4ca5bcb4737478fddfaccdb8886cf5d4c252318
Abraham Mamani <abraham.e.mamani@gmail.com>
2019-09-09 01:34:08 -0300

    Updates

---
+++ precache-manifest.3985651e8a5b1ae1d71c93aa0f937937.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479c0dd4fb277c2d82a1b504396e25da",
+    "url": "/entype/index.html"
+  },
+  {
+    "revision": "f93a8c32b0564f931709",
+    "url": "/entype/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d07a05fbd74a2940d39b",
+    "url": "/entype/static/css/main.de1d3b1f.chunk.css"
+  },
+  {
+    "revision": "f93a8c32b0564f931709",
+    "url": "/entype/static/js/2.fda439a8.chunk.js"
+  },
+  {
+    "revision": "d07a05fbd74a2940d39b",
+    "url": "/entype/static/js/main.9b653d6c.chunk.js"
+  },
+  {
+    "revision": "e6b99d1c027c0d19ea30",
+    "url": "/entype/static/js/runtime~main.4b507f17.js"
+  },
+  {
+    "revision": "7f80aeae1b0de3ad98e8893512d47ef1",
+    "url": "/entype/static/media/background.7f80aeae.jpg"
+  }
+]);
